8d335a919b2f94883e7c2b75cf834cf050a67b6b
evgeni83 <evgeni.daniluk@yandex.ru>
2019-08-11 11:27:48 +0300

    Canged ES5 to ES6

--- scriptES6.js
+++ scriptES6.js
@@ -1,42 +1,38 @@
 class Vehicle {
   constructor(mark, model, fuelCapacity, fuelСonsumption) {
-    this.military = false;
     this.mark = mark;
     this.model = model;
     this.fuelCapacity = fuelCapacity;
     this.fuelСonsumption = fuelСonsumption;
-  }
-  
-  cruisingRange() { 
-    return ( parseInt(this.fuelCapacity) / parseInt(this.fuelСonsumption)) * 100
-  }
+    this.cruisingRange = () => (parseInt(this.fuelCapacity) / parseInt(this.fuelСonsumption)) * 100;
+  }  
 
-  message() {
+  message(military = false) {
     console.log(`${this.type} ${this.mark} ${this.model} при полной заправке ${this.movingMethod} ${this.cruisingRange()} километров.`);
-    
-    if (this.military) {
+
+    if (military) {
       console.log(`${this.mark} ${this.model} - это военный ${this.type}`);
     }
   }
 };
 
 class Car extends Vehicle {
-  constructor(mark, model, fuelCapacity, fuelСonsumption) {
-    super(mark, model, fuelCapacity, fuelСonsumption);
+  constructor(...rest) {
+    super(...rest);
     this.type = 'автомобиль';
     this.movingMethod = 'проезжает';
   }
 };
 class Plane extends Vehicle {
-  constructor(mark, model, fuelCapacity, fuelСonsumption) {
-    super(mark, model, fuelCapacity, fuelСonsumption);
+  constructor(...rest) {
+    super(...rest);
     this.type = 'самолет';
     this.movingMethod = 'пролетает';
   }
 };
 class Ship extends Vehicle {
-  constructor(mark, model, fuelCapacity, fuelСonsumption) {
-    super(mark, model, fuelCapacity, fuelСonsumption);
+  constructor(...rest) {
+    super(...rest);
     this.type = 'корабль';
     this.movingMethod = 'проходит';
   }
@@ -47,9 +43,8 @@ const car = new Car('BMW', 'X5', 50, 10);
 car.message();
 
 const jet = new Plane('Su', '27', 3000, 50);
-jet.military = true;
 
-jet.message();
+jet.message(true);
 
 const boat = new Ship('Yamaha', '2000', 150, 40);
 
